0a584002e268bd04ed36919dddb233cd9ff5f253
Jerry <jerrynutt416@gmail.com>
2022-11-24 22:43:29 -0500

    Update

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -1,27 +1,42 @@
 import '../styles/sidebar.css';
 import { useSelector } from 'react-redux';
-
+import { useLayoutEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { sidebarVisibility } from '../features/sidebarvisibility';
 
 function Sidebar() {
+  //const [isMobile, setIsMobile] = useState(false);
   const visibility = useSelector((state) => state.sidebarvisibility.value);
   const dispatch = useDispatch();
-  console.log(visibility);
+  //console.log(visibility);
   const handleClick = () => {
-    // if (sidebarb) {
-    dispatch(sidebarVisibility.setsidebarData('sidebarHidden'));
-    //  return document
-    //   .querySelector('#change-class')
-    //  .classList.add('sidebarHidden');
-    //}
-    // console.log('its false');
-    // dispatch(sidebarVisibility.setsidebarData(true));
-    // return document
-    //  .querySelector('#change-class')
-    //  .classList.remove('sidebarHidden');
+    if (window.innerWidth < 720) {
+      console.log('small');
+      dispatch(sidebarVisibility.setsidebarData('mobileHidden'));
+    } else {
+      console.log('large');
+      dispatch(sidebarVisibility.setsidebarData('sidebarHidden'));
+    }
   };
 
+  useLayoutEffect(() => {
+    function updateSize() {
+      if (visibility === 'sidebarVisible' || visibility === 'sidebarMobile') {
+        if (window.innerWidth < 720) {
+          dispatch(sidebarVisibility.setsidebarData('sidebarMobile'));
+        } else {
+          dispatch(sidebarVisibility.setsidebarData('sidebarVisible'));
+        }
+      } else if (visibility === 'sidebarHidden' && window.innerWidth < 720) {
+        dispatch(sidebarVisibility.setsidebarData('sidebarMobile'));
+      }
+    }
+    window.addEventListener('resize', updateSize);
+    updateSize();
+    return () => window.removeEventListener('resize', updateSize);
+  }, [visibility, dispatch]);
+
+  ///
   return (
     <div className={visibility} id="change-class">
       <div className="logo">
